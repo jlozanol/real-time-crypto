007b644a814e1be8853d35ecc2dc9b5f40d52021
“jlozanol” <jorchlozano@gmail.com>
2024-11-22 11:08:19 -0600

    Add pydantic-settings for the configuration
    Added pydantic-settings to automatically check config types and catch errors on the fly.

--- services/trade_producer/src/config.py
+++ services/trade_producer/src/config.py
@@ -1,11 +1,17 @@
 import os
 
 from dotenv import find_dotenv, load_dotenv
+from pydantic_settings import BaseSettings
 
 # load my .env file variables as environment variables so I can access them
 # with os.environ[] statements
 load_dotenv(find_dotenv())
 
-kafka_broker_address = os.environ['KAFKA_BROKER_ADDRESS']
-kafka_topic_name = 'trade'
-product_id = 'BTC/USD'
+
+class Config(BaseSettings):
+	product_id: str = 'ETH/USD'
+	kafka_broker_address: str = os.environ['KAFKA_BROKER_ADDRESS']
+	kafka_topic_name: str = 'trade'
+
+
+config = Config()
--- services/trade_producer/src/main.py
+++ services/trade_producer/src/main.py
@@ -4,7 +4,7 @@ from typing import Dict, List
 from loguru import logger
 from quixstreams import Application
 
-from src import config
+from src.config import config
 from src.kraken_api import KrakenWebsocketTradeAPI
 
 
